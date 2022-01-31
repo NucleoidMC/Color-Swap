bc42f838ae6a1ba14a3bb01565a2f72ae74af52a
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-01-31 13:01:30 -0500

    Update minigame to Minecraft 1.18.1

--- src/main/java/io/github/haykam821/colorswap/game/map/ColorSwapMapConfig.java
+++ src/main/java/io/github/haykam821/colorswap/game/map/ColorSwapMapConfig.java
@@ -10,7 +10,6 @@ import net.minecraft.block.Blocks;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
-import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 
 public class ColorSwapMapConfig {
 	public static final Codec<ColorSwapMapConfig> CODEC = RecordCodecBuilder.create(instance -> {
@@ -19,8 +18,8 @@ public class ColorSwapMapConfig {
 				Codec.INT.fieldOf("z").forGetter(config -> config.z),
 				Codec.INT.optionalFieldOf("x_scale", 3).forGetter(config -> config.xScale),
 				Codec.INT.optionalFieldOf("z_scale", 3).forGetter(config -> config.zScale),
-				BlockStateProvider.TYPE_CODEC.optionalFieldOf("initial_state_provider", new SimpleBlockStateProvider(Blocks.WHITE_WOOL.getDefaultState())).forGetter(config -> config.initialStateProvider),
-				BlockStateProvider.TYPE_CODEC.optionalFieldOf("erased_state_provider", new SimpleBlockStateProvider(Blocks.AIR.getDefaultState())).forGetter(config -> config.erasedStateProvider),
+				BlockStateProvider.TYPE_CODEC.optionalFieldOf("initial_state_provider", BlockStateProvider.of(Blocks.WHITE_WOOL)).forGetter(config -> config.initialStateProvider),
+				BlockStateProvider.TYPE_CODEC.optionalFieldOf("erased_state_provider", BlockStateProvider.of(Blocks.AIR)).forGetter(config -> config.erasedStateProvider),
 				Identifier.CODEC.fieldOf("platformBlocks").forGetter(config -> config.platformBlocks)
 		).apply(instance, ColorSwapMapConfig::new);
 	});
